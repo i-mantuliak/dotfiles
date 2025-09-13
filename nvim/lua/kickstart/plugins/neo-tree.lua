58bef1a836e19950ac257479a1796c214d09ffe4
Igor Mantuliak <igor.mantuliak@my.games>
2025-09-14 01:31:26 +0200

    update

--- nvim/lua/kickstart/plugins/neo-tree.lua
+++ nvim/lua/kickstart/plugins/neo-tree.lua
@@ -14,6 +14,7 @@ return {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
+    add_blank_line_at_top = true,
     close_if_last_window = true,
     filesystem = {
       window = {
